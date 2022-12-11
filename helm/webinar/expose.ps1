1233e59e713cdbf3e4232b5bb051224b51ddfab4
Csendes Dávid <csendes.david@alerant.hu>
2022-12-11 16:04:37 +0100

    Added scripts + added traceID for logs

---
+++ helm/webinar/expose.ps1
@@ -0,0 +1,1 @@
+kubectl port-forward svc/webinar-api 8080:8080 
--- local/db.ps1
+++ local/db.ps1
@@ -1,3 +1,3 @@
 java -jar $DB_WORKSPACE\target\webuni-webinar-db-demo-0.0.1-SNAPSHOT.jar `
-    --server.port=8081 `
+    --server.port=8081 --management.server.port=9081 `
     --spring.datasource.url=jdbc:postgresql://localhost:5432/postgres --spring.datasource.username=user --spring.datasource.password=password
